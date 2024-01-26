24f2755038276ae7caa86bcb517db06cc995cd96
Karla C. Ferreira <karlinharural@gmail.com>
2024-01-26 14:30:19 -0300

    Create phishing.sh

---
+++ phishing.sh
@@ -0,0 +1,12 @@
+Neste desafio de projeto, iremos criar um Phishing para capturar senhas de login do Facebook.
+
+USA-SE O KALI
+
+sudo su
+setoolkit
+Social-Engineering Attacks
+Web Site Attack Vectors
+Credential Harvester Attack Method 
+Site Cloner
+ifconfig
+https://www.facebook.com/
